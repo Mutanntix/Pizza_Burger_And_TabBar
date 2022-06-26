daeb58a5ec1e87bac371ee0ff7068af481d76fee
mutanntix <gumus96@mail.ru>
2022-06-26 20:03:06 +0300

    Customized MainTabBarController

--- Lesson11/MainTabBarController.swift
+++ Lesson11/MainTabBarController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+    let tabBarTopLine = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tabBar.backgroundColor = UIColor(red: 100/255, green: 125/255, blue: 210/255, alpha: 0.2)
+        self.tabBar.tintColor = .darkGray
         
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupTopLine()
+    }
+}
+
+//MARK: UI
+extension MainTabBarController {
+    func setupViewControllers() {
+        let pizzaImg = UIImage(named: "pizza2")!
+        let burgerImg = UIImage(named: "burger2")!
+        let selectedBurgerImg = UIImage(named: "burgerSelected")!
+        let selectedPizzaImg = UIImage(named: "pizzaSelected")
+        
+        viewControllers = [
+            createNavController(for: BurgerMainViewController(), title: "Burgers", image: burgerImg, selectedImage: selectedBurgerImg ),
+            createNavController(for: PizzaMainViewController(), title: "Pizza", image: pizzaImg, selectedImage: selectedPizzaImg)
+        ]
     }
     
     fileprivate func createNavController(for
@@ -29,16 +52,19 @@ class MainTabBarController: UITabBarController {
         
         return navController
     }
-    
-    func setupViewControllers() {
-        let pizzaImg = UIImage(named: "pizza2")!
-        let burgerImg = UIImage(named: "burger2")!
-        let selectedBurgerImg = UIImage(named: "burgerSelected")!
-        
-        viewControllers = [
-            createNavController(for: BurgerMainViewController(), title: "Burgers", image: burgerImg, selectedImage: selectedBurgerImg ),
-            createNavController(for: PizzaMainViewController(), title: "Pizza", image: pizzaImg)
-        ]
-    }
+}
 
+//MARK: Methods
+extension MainTabBarController {
+    private func setupTopLine() {
+        tabBarTopLine.backgroundColor = .darkGray
+        let origin = CGPoint(x: 0,
+                             y: tabBar.frame.minY + 0.2)
+        let size = CGSize(width: tabBar.frame.width,
+                          height: 0.2)
+        tabBarTopLine.frame = CGRect(origin: origin,
+                                     size: size)
+        
+        view.addSubview(tabBarTopLine)
+    } 
 }
